15f4d55d7fe73c04b3fc094e527f439c0b1b8bec
PasquiFra <francesco.pasquinoni@gmail.com>
2024-02-01 17:55:58 +0100

    header OK

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,124 @@
 <script>
 
 export default {
-    name: "Header"
+    name: "Header",
+    data: () => ({
+        navs: [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+        ]
+    })
 }
 </script>
 
 <template>
-    <h1>header</h1>
+    <header class="flex justify-header">
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </figure>
+        <nav class="flex center">
+            <ul class="flex">
+                <li class="flex center list-setup" v-for="nav in navs">
+                    <a class="link-setup flex center" href="{{ nav.url }}">
+                        {{ nav.text }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
-<style></style>
+<style scoped>
+header {
+    height: 100px;
+}
+
+figure {
+    height: 75px;
+    align-self: center;
+}
+
+img {
+    height: 100%;
+    width: auto;
+}
+
+.justify-header {
+    justify-content: space-between;
+}
+
+.list-setup {
+    list-style-type: none;
+}
+
+.link-setup {
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 0.8rem;
+
+    color: rgba(48, 48, 48, 0.788);
+
+    margin: 0 1rem;
+}
+
+ul,
+a {
+    height: 100%;
+}
+
+ul a:hover,
+ul a:active {
+
+    border-bottom: 5px solid dodgerblue;
+    color: dodgerblue;
+}
+</style>
